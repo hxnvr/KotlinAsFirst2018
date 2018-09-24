70233941f65e9056e3789853e3d552b608a38657
hxnvr <hxnvr@mail.ru>
2018-09-24 14:03:41 +0300

    lesson 2 done

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,6 +3,7 @@ package lesson2.task1
 
 import lesson1.task1.discriminant
 import lesson1.task1.sqr
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.sqrt
 /**
@@ -159,4 +160,10 @@ fun triangleKind(a: Double, b: Double, c: Double): Int {
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = when {
+    c > b || a > d -> -1
+    a <= c && b <= d -> abs(b - c)
+    a <= c && d <= b -> abs(d - c)
+    c <= a && b <= d -> abs(b - a)
+    else -> abs(d - a)
+}
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@ package lesson2.task2
 
 import lesson1.task1.sqr
 import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -57,7 +58,10 @@ fun daysInMonth(month: Int, year: Int): Int {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean = when {
+    sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2 -> true
+    else -> false
+}
 
 /**
  * Средняя
@@ -68,4 +72,11 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = when {
+    (r >= a) && (s >= b || s >= c) -> true
+    (r >= b) && (s >= a || s >= c) -> true
+    (r >= c) && (s >= b || s >= a) -> true
+    else -> false
+
+
+}
