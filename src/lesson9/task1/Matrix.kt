5a2f752eb0d7ee13891c7870d36c7153c11d3458
hxnvr <hxnvr@mail.ru>
2019-01-25 20:05:31 +0300

    !

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -1,6 +1,8 @@
 @file:Suppress("UNUSED_PARAMETER", "unused")
 package lesson9.task1
 
+import java.lang.IllegalArgumentException
+
 /**
  * Ячейка матрицы: row = ряд, column = колонка
  */
@@ -38,32 +40,57 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
-
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if ((height <= 0) || (width <= 0)) throw IllegalArgumentException()
+    return MatrixImpl(height, width, e)
+}
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E> (override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    val array = MutableList(height) { MutableList(width) { e } }
 
-    override val width: Int = TODO()
+    override fun get(row: Int, column: Int): E  = array[row][column]
 
-    override fun get(row: Int, column: Int): E  = TODO()
-
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E  = get(cell.row, cell.column)
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        array[row][column] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        array[cell.row][cell.column] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?): Boolean {
+        if ((other !is MatrixImpl<*>) || (height != other.height) || (width != other.width)) return false
+        for (i in 0 until height)
+            for (j in 0 until width)
+                if (array[i][j] != other[i, j]) return false
+        return true
+    }
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0 until height) {
+            sb.append("[")
+            for (column in 0 until width) {
+                sb.append(this[row, column], " ")
+            }
+            sb.append("]")
+        }
+        sb.append("]")
+        return "$sb"
+    }
+
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        return result
+    }
 }
 
